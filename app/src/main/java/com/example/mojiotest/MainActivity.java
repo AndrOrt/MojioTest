1481944c295648138505b36018072ee1b16a8bad
Andrei Ortyashov <Andrei_Ortyashov@epam.com>
2017-02-01 12:26:17 +0300

    Login

--- app/src/main/java/com/example/mojiotest/MainActivity.java
+++ app/src/main/java/com/example/mojiotest/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.mojiotest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import io.moj.java.sdk.MojioClient;
+import io.moj.java.sdk.model.User;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    App app;
+    FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        app = ((App) getApplicationContext());
+        final MojioClient mojioClient = app.getMojioClient();
+
+        mojioClient.login("andr-ort@mail.ru", "EpamMojio").enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                Toast.makeText(getApplicationContext(), response.body().getUserName(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), "Error Login", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
